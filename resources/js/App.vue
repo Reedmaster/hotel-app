1a55eb80c30111e23a86a4c1ff1c1fff00f50c61
reedmaster <arspare22496@gmail.com>
2023-06-02 16:51:36 +0100

    add router link

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="text-red-500 text-3xl">Welcome page with vue!</div>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
   <router-view></router-view>
 </template>
 
