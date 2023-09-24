3fec8caf2ed685123e043e5486d90a80b5163499
reedmaster <arspare22496@gmail.com>
2023-09-24 16:27:44 +0100

    change text

--- resources/js/components/UI/Navigation/NavInfo.vue
+++ resources/js/components/UI/Navigation/NavInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-10 items-center justify-end pr-10 opacity-50 bg-priory-red text-priory-cream font-cardo">
-    Hello
+    Book
     <Icon icon="arrow-right-icon" width="20" height="20" color="fill-priory-cream" />
   </div>
 </template>
